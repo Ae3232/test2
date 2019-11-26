a499fe57c7adf2e44886bff4f8bd1c56eb2f9974
aerend <narsil1@yandex.ru>
2019-11-26 22:47:08 +0300

    main-v1

--- src/js/main.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-	$(".intro_arrow").click(function() {
-	    $([document.documentElement, document.body]).animate({
-	        scrollTop: $(".sorting").offset().top
-	    }, 700);
-	});
-});
